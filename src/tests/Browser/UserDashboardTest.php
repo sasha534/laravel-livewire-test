b8084637c38da46c690ec3066ccd027989cfb039
Oleksandr Sorokin <oleksandr1sorokin@gmail.com>
2023-01-30 07:47:16 +0200

    add tests

---
+++ src/tests/Browser/UserDashboardTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class UserDashboardTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+    
+    /**
+     * A Dusk test example.
+     */
+    public function testExample(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/user-dashboard')
+                    ->assertSee('Chose pagination');
+        });
+    }
+}
